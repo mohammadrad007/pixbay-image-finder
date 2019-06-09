8a491409b77f0a6c29737cf626571284efae200f
Mohammad Mohammadi <saberzx313@gmail.com>
2019-06-09 15:23:33 +0430

    search component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 import NavBar from "./components/navbar/NavBar";
+import Search from "./components/search/Search";
 
 class App extends Component {
   render() {
     return (
       <MuiThemeProvider>
         <NavBar />
+        <Search />
       </MuiThemeProvider>
     );
   }
