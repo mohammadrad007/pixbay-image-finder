ee2293ab65b0d42a1e335b534cde941738997c16
Mohammad Mohammadi <saberzx313@gmail.com>
2019-06-09 15:27:44 +0430

    add propTypes to Images Result component

---
+++ src/components/image-results/ImageResults.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+class ImageResults extends Component {
+  render() {
+    return <div />;
+  }
+}
+ImageResults.prototype = {
+  images: PropTypes.array.isRequired
+};
+export default ImageResults;
--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -61,7 +61,7 @@ class Search extends Component {
           <MenuItem value={50} primaryText="50" />
         </SelectField>
         <br />
-
+    {this.state.images.length > 0 ? (<ImageResults images={this.state.images}/>) :(<p>Search Something</p>))}
       </div>
     );
   }
