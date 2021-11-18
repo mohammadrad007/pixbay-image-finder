df9589090e47ddc57f78c0a264e93f4ab6e7464b
unknown <saberzx313@gmail.com>
2021-11-18 15:19:49 +0330

    develope-sidebar-and-responsive

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import React, { Component } from "react";
-import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 import NavBar from "./components/navbar/NavBar";
 import Search from "./components/search/Search";
 
+import classes from "./App.module.css";
 class App extends Component {
   render() {
     return (
-      <MuiThemeProvider>
-        <>
-          <NavBar />
-          <Search />
-        </>
-      </MuiThemeProvider>
+      <div className={classes.app}>
+        <NavBar />
+        <Search />
+      </div>
     );
   }
 }
--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -2,6 +2,9 @@ import React from "react";
 import classes from "./navbar.module.css";
 
 const NavBar = () => (
-  <div className={classes.navbar}>PixBay Image Finder Version 2</div>
+  <div className={classes.navbar}>
+    <h2>PixBay Image Finder </h2>
+    <span>Version 2</span>
+  </div>
 );
 export default NavBar;
