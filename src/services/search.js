c4f46e1fdde886d8917916e6d5eef30e7997cbaf
mohammad mohammadi <mohammad.mohamadi0101@gmail.com>
2022-02-05 18:19:49 +0330

    clean

--- src/services/search.js
+++ src/services/search.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 
-const apiKey = "12725301-6d1d3080ea8ea5f1d3bde1efa";
-const apiUrl = "https://pixabay.com/api";
+const API_KEY = "12725301-6d1d3080ea8ea5f1d3bde1efa";
+const BASE_URL = "https://pixabay.com/api";
 
 export const getImageFromPixbay = async (text, amount) => {
   const data = await axios
     .get(
-      `${apiUrl}/?key=${apiKey}&q=${text}&image_type=photo&per_page=${amount}&safesearch=true`
+      `${BASE_URL}/?key=${API_KEY}&q=${text}&image_type=photo&per_page=${amount}&safesearch=true`
     )
     .then((res) => res)
     .catch((err) => console.log(err));
